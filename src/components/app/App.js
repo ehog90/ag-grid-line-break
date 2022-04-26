590d212bb3ded1151cebc66dbc9fa78cf55c3a2b
Bence Ujj <bence.ujj@lab49.com>
2022-04-26 16:50:38 +0200

    Add version flag

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <Fragment>
-      <header>Ag-Grid line-break test</header>
+      <header>Ag-Grid line-break test (V25)</header>
       <main>
         <div className="container">
           <div className="ag-theme-alpine">
